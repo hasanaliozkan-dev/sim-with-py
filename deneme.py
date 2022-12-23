5286e4ee5c91b69dcd8878aee784ded0db0682d6
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2022-12-23 09:43:44 +0300

    "Term finished."

---
+++ deneme.py
@@ -0,0 +1,3 @@
+montly_weight = {"January":1.5,"February":0.6,"March":1.3,"April":0.9,"May":1.6,"June":1.1,"July":0.3,"August":0.3,"September":0.7,"October":1.0,"November":1.4,"December":0.9}
+for i in montly_weight:
+    print(i, montly_weight[i])
